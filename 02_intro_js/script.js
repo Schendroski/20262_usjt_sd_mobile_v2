aeb71cf3da11662ae28a0b4fbdab1f4cb2750c07
Livia de Queiroz Schendroski <824217515@ulife.com.br>
2026-08-20 21:13:52 -0300

    learn(ap01-js): encerra codigo 1.1.2

---
+++ 02_intro_js/script.js
@@ -0,0 +1,20 @@
+// declaração de variáveis e constantes
+// const, let e var
+var idade = 18
+var nome
+console.log(`Oi, ${nome}`)
+if(idade >= 18){
+    var nome = "José"
+    console.log(`Parabéns ${nome}. Você pode dirigir`)
+}
+console.log(`Até, ${nome}`)
+// var linguagem = "Java"
+// console.log("Aprendendo" + linguagem)
+// var linguagem = "Javascript"
+// console.log('Aprendendo ${linguagem}')
+// const nome = 'José'
+// nome = "José Silva"
+// let idade = 17
+// idade = 18
+// var sobrenome = "Soares"
+// sobrenome = "Silva"
