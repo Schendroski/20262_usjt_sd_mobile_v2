50c6313a46080de23eec3d9e9e42f8ca5ee6c0e3
Livia de Queiroz Schendroski <824217515@ulife.com.br>
2026-08-20 21:45:55 -0300

    learn(ap01-js): encerra codigo 1.3.1

--- 02_intro_js/script.js
+++ 02_intro_js/script.js
@@ -1,13 +1,23 @@
+//coerção
+//implícita, explícita
+const n1 = 2
+const n2 = '3'
+//coerção implícita
+const n3 = n1 + n2
+console.log(n3)
+//coerção explícita
+const n4 = n1 + Number(n2)
+console.log(n4)
 // declaração de variáveis e constantes
 // const, let e var
-var idade = 18
-var nome
-console.log(`Oi, ${nome}`)
-if(idade >= 18){
-    var nome = "José"
-    console.log(`Parabéns ${nome}. Você pode dirigir`)
-}
-console.log(`Até, ${nome}`)
+// var idade = 18
+// var nome
+// console.log(`Oi, ${nome}`)
+// if(idade >= 18){
+//     var nome = "José"
+//     console.log(`Parabéns ${nome}. Você pode dirigir`)
+// }
+// console.log(`Até, ${nome}`)
 // var linguagem = "Java"
 // console.log("Aprendendo" + linguagem)
 // var linguagem = "Javascript"
